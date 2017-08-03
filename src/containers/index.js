0d60f7ccb75e3fdc80637490fa7586bab96a3b28
Steve Mckellar <steve@wwqrd.uk>
2017-08-03 10:48:10 +0100

    Remove references in front-end to tray

--- src/containers/index.js
+++ src/containers/index.js
@@ -1,5 +1,4 @@
 export { default as App } from './App';
 export { default as ExportScreen } from './ExportScreen';
 export { default as OverviewScreen } from './OverviewScreen';
-export { default as Tray } from './Tray';
 export { default as icp } from './ipc';
