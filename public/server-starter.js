e8691130738d069fac6d485992c62e6e09c79a80
Steve Mckellar <steve@wwqrd.uk>
2017-09-28 10:03:46 +0100

    Get server status in server starter, seems to run okay

--- public/server-starter.js
+++ public/server-starter.js
@@ -1,3 +1,12 @@
 const { createServer } = require('./components/serverManager');
 
-createServer(8080);
+createServer(8080).then((serverProcess) => {
+  serverProcess.on(
+    'SERVER_STATUS',
+    ({ data }) => {
+      console.log(data);
+    }
+  );
+
+  serverProcess.send({ action: 'REQUEST_SERVER_STATUS' });
+});
