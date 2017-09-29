772c3dcf80ba3466f3181e75232a1ee2cfaff3e6
Steve Mckellar <steve@wwqrd.uk>
2017-09-29 12:05:32 +0100

    Remove cordova work arounds

--- src/components/ServerPanel.js
+++ src/components/ServerPanel.js
@@ -1,9 +1,6 @@
 import React, { Component } from 'react';
 import PropTypes from 'prop-types';
 import { PanelItem } from '../components';
-import Updater from '../utils/Updater';
-
-const updater = new Updater();
 
 const defaultServerOverview = {
   ip: 'x.x.x.x',
@@ -20,14 +17,6 @@ class ServerPanel extends Component {
       version: '0.0.0',
       notes: '',
     };
-
-    updater.on('UPDATE_AVAILABLE', ({ version, notes }) => {
-      this.setState({
-        ...defaultServerOverview,
-        version,
-        notes,
-      });
-    });
   }
 
   render() {
